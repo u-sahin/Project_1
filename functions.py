f11ee13aabd5c0ac837583bba5e1a03f839a25d8
u-sahin <uemitsahin@outlook.de>
2021-05-24 20:06:08 +0200

    Naming fix
    Fixed some naming-conventions for the functions in functions.py

--- functions.py
+++ functions.py
@@ -3,29 +3,29 @@ import numpy as np
 import pandas as pd
 
 
-def create_mixed_plot(df, group, measure, measure_2, title):
+def create_mixed_plot(dataframe, groupby, primary_measure, secondary_measure, title):
     """
     Description: This function can be used to create a plot with 2 measures
 
     Arguments:
         df: the dataframe
         group: columns to be grouped of the dataframe (can be multiple ones)
-        measure: the first measure (will be on the secondary_y axis)
-        measure_2: the second measure
+        primary_measure: the primary measure
+        secondary_measure: the secondary measure (will be on the secondary_y axis)
         title: the title for the plot
         
     Returns:
         The plot
     """
-    ax = df.groupby(group)[measure].mean().plot(secondary_y=True, color="r", ylabel=measure_2, rot=90, legend=True)
-    df.groupby(group)[measure_2].mean().plot.bar(ylabel=measure, ax=ax, color='tab:blue', edgecolor="k", legend=True)
+    ax = dataframe.groupby(groupby)[secondary_measure].mean().plot(secondary_y=True, color="r", ylabel=primary_measure, rot=90, legend=True)
+    dataframe.groupby(groupby)[primary_measure].mean().plot.bar(ylabel=secondary_measure, ax=ax, color='tab:blue', edgecolor="k", legend=True)
     
     plt.title(title)
 
     return plt
 
 
-def create_mean_and_count_plot(df, group, measure, ylabel, title):
+def create_mean_and_count_plot(dataframe, groupby, measure, secondary_ylabel, title):
     """
     Description: This function can be used to create a plot with 1 measure and a count as a secondary_y axis
 
@@ -39,8 +39,8 @@ def create_mean_and_count_plot(df, group, measure, ylabel, title):
     Returns:
         The plot
     """
-    ax = df.groupby(group).count().plot(secondary_y=True, color="r", ylabel=measure, rot=90, legend=False)
-    df.groupby(group)[measure].mean().plot.bar(ylabel=ylabel, ax=ax, color='tab:blue', edgecolor="k", legend=True)
+    ax = dataframe.groupby(groupby).count().plot(secondary_y=True, color="r", ylabel=measure, rot=90, legend=False)
+    dataframe.groupby(groupby)[measure].mean().plot.bar(ylabel=secondary_ylabel, ax=ax, color='tab:blue', edgecolor="k", legend=True)
     
     plt.title(title)
 
